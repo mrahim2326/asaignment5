301762c60bfb97e417ed7a3150f6f1045891c408
mrahim2326 <rahimrowshan239142@gmail.com>
2024-10-15 19:11:33 +0600

    thirdpush

---
+++ script/fetures.js
@@ -0,0 +1,11 @@
+document.getElementById('blog').addEventListener('click',
+    function(event){
+        event.preventDefault();
+        window.location.href = '/index.html'
+
+})
+// document.getElementById('home').addEventListener('click',
+//     function(){
+//      window.location.href='../index.html'
+//     }
+// )
