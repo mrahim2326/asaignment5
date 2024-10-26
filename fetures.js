fa6c8130f1b76995d1505de175cff003c8859cd7
mrahim2326 <rahimrowshan239142@gmail.com>
2024-10-26 12:57:33 +0600

    push agine

---
+++ fetures.js
@@ -0,0 +1,16 @@
+// document.getElementById('blog').addEventListener('click',
+//     function(event){
+//         event.preventDefault();
+//         window.location.href = '/fAndQ.html'
+
+// })
+// document.getElementById('home').addEventListener('click',
+//     function(){
+//      window.location.href='../index.html'
+//     }
+// )
+document.getElementById('blog').addEventListener('click',
+    function(){
+        window.location.href='fAndQ.html'
+    }
+)
