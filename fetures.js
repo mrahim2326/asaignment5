ce8fc62fb37517c41c93c7730da9c1fd03962065
mrahim2326 <rahimrowshan239142@gmail.com>
2024-12-29 11:04:16 +0600

    again

--- fetures.js
+++ fetures.js
@@ -1,16 +1,20 @@
-// document.getElementById('blog').addEventListener('click',
-//     function(event){
-//         event.preventDefault();
-//         window.location.href = '/fAndQ.html'
 
-// })
-// document.getElementById('home').addEventListener('click',
-//     function(){
-//      window.location.href='../index.html'
-//     }
-// )
 document.getElementById('blog').addEventListener('click',
     function(){
         window.location.href='fAndQ.html'
     }
 )
+
+document.getElementById('history').addEventListener('click',
+    function(){
+    document.getElementById('donation').style.backgroundColor="white";
+    document.getElementById('history').style.backgroundColor="#B4F461";
+
+})
+document.getElementById('donation').addEventListener('click',
+    function(){
+    document.getElementById('history').style.backgroundColor="white";
+    document.getElementById('donation').style.backgroundColor="#B4F461";
+
+})
+
